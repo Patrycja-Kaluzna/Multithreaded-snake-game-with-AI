51bae5678105ad84b2375bbcf257e1e33cc8546c
Ernest Torz <ernesttorz@gmail.com>
2022-05-13 18:57:36 +0200

    new

--- src/application/IntroSceneControl.java
+++ src/application/IntroSceneControl.java
@@ -3,12 +3,15 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class IntroSceneControl {
     @FXML
     public static AnchorPane IntroSceneParent;
+    @FXML
+    private Button EXIT_Button;
 
     //Start button handler
     public void game_click(ActionEvent actionEvent) {
@@ -16,4 +19,11 @@ public class IntroSceneControl {
         double size= (double) (((Node)actionEvent.getSource()).getScene().getHeight());
         game.Game_Start((Stage)((Node)actionEvent.getSource()).getScene().getWindow(),size);
     }
+    public void EXIT(ActionEvent actionEvent) {
+        Stage stage;
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+
+    }
+
 }
