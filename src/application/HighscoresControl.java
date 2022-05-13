2fb9af2dc49c8d02fcfec8a8bae84c45a85bb576
Ernest Torz <ernesttorz@gmail.com>
2022-05-13 21:28:50 +0200

    new

--- src/application/HighscoresControl.java
+++ src/application/HighscoresControl.java
@@ -1,10 +1,39 @@
 package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.io.IOException;
 
 public class HighscoresControl {
+    @FXML
+    private Text Player1;
+   @FXML
+   public void initialize() throws FileNotFoundException {
+       File file = new File("C:\\Users\\Ernest\\IdeaProjects\\Multithreaded-snake-game-with-AI\\src\\application\\Highscores.txt");
+       Scanner sc =new Scanner(file);
+
+       Player1.setText(sc.nextLine());
+   }
+
+    public void EXIT_Highscores(ActionEvent actionEvent) throws IOException {
+        Stage stage;
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("IntroScene.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- src/application/IntroSceneControl.java
+++ src/application/IntroSceneControl.java
@@ -2,10 +2,16 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class IntroSceneControl {
     @FXML
@@ -23,6 +29,17 @@ public class IntroSceneControl {
         Stage stage;
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         stage.close();
+
+    }
+
+    public void Highscores_click (ActionEvent actionEvent) throws IOException {
+        Stage stage;
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("HighscoresScene.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
 
