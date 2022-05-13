338d2c3bc5fd73532a2bc4906e6764af3115e082
Ernest Torz <ernesttorz@gmail.com>
2022-05-13 19:06:58 +0200

    new

---
+++ src/application/HighscoresControl.java
@@ -0,0 +1,10 @@
+package application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class HighscoresControl {
+}
